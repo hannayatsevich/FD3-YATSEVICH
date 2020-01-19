b531e50a91c88f95846aa4bb614f478d59645706
hannayatsevich <hannayatsevich@gmail.com>
2020-01-19 19:32:36 +0300

    Mobile created

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,59 @@
+'use stict';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MobileClient.css'
+import { mobileEvents } from './events';
+
+class MobileClient extends React.PureComponent {
+  static propTypes = {
+    clientDetails: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      provider: PropTypes.string.isRequired,
+      firstName: PropTypes.string.isRequired,
+      secondName: PropTypes.string.isRequired,
+      tarrifPlan: PropTypes.string.isRequired,
+      balance: PropTypes.number.isRequired,
+    })    
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientDetails: props.clientDetails,
+    };
+  };
+
+  deleteClient = () => {
+    let deleteAnswer = confirm(`Do you want to delete ${this.state.clientDetails.firstName} ${this.state.clientDetails.secondName} data?`);
+    if(deleteAnswer) {
+      mobileEvents.emit('EDeleteClient', this.state.clientDetails.id)
+    };    
+  };
+  editClient = () => {
+    mobileEvents.emit('EEditClient', this.state.clientDetails.id)
+  };
+
+  componentWillReceiveProps = (newProps) => {
+    this.setState({clientDetails: newProps.clientDetails});
+  };
+
+  render() {
+    console.log(`MobileClient ${this.state.clientDetails.id} render`);
+    return(
+    <tr className = 'MobileClient'>
+      <td>{this.state.clientDetails.provider}</td>
+      <td>{this.state.clientDetails.firstName}</td>
+      <td>{this.state.clientDetails.secondName}</td>
+      <td>{this.state.clientDetails.tarrifPlan}</td>
+      <td>{this.state.clientDetails.balance}</td>
+      <td className = {this.state.clientDetails.balance <= 0 ? 'BlockedRed' : 'ActiveGreen'}>
+        {this.state.clientDetails.balance <= 0 ? 'blocked' : 'active'}
+      </td>
+      <td><input type = 'button' value = 'Edit' onClick = {this.editClient}/></td>
+      <td><input type = 'button' value = 'Delete' onClick = {this.deleteClient}/></td>
+    </tr>
+    );
+  };
+};
+
+export default MobileClient;
